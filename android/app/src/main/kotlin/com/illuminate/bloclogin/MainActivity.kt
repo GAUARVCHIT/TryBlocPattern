783aebde32ddbd88d5cbfd8e5ef7a19e0f326f84
GAURAVCHIT <gaurav198.rr@gmail.com>
2021-01-03 16:09:49 +0530

    remaining files Upto single global instance

---
+++ android/app/src/main/kotlin/com/illuminate/bloclogin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.illuminate.bloclogin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          emailField(),
+          passwordField(),
+          Container(
+            margin: EdgeInsets.only(top: 25.0),
+          ),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return StreamBuilder(
+      stream: bloc.email,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changeEmail,
+          decoration: InputDecoration(
+            hintText: 'gaurav198.rr@gmail.com',
+            labelText: 'Email',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget passwordField() {
+    return StreamBuilder(
+      stream: bloc.password,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: bloc.changePassword,
+          decoration: InputDecoration(
+            hintText: 'Password',
+            labelText: 'Password',
+            errorText: snapshot.error,
+          ),
+        );
+      },
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
