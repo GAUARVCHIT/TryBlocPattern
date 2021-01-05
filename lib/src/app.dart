1dc92e114c72a81e4d386b490c6bb62c98370be2
GAURAVCHIT <gaurav198.rr@gmail.com>
2021-01-05 15:27:36 +0530

    project complete

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import './blocs/provider.dart';
 
-class App extends StatelessWidget{
-  build(context){
+class App extends StatelessWidget {
+  build(context) {
+    return Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen(),
+        ),
+      ),
+    );
     return MaterialApp(
       title: 'Log Me In',
       home: Scaffold(
